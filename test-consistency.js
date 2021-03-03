1b081d76b9c46c701f9207d7db0b25f62e893bb8
ccorsin <ccorsin@student.42.fr>
2021-03-03 17:21:16 +0100

    Fix test

--- test-consistency.js
+++ test-consistency.js
@@ -3,13 +3,13 @@ const data = require("./data.js")
 
 try {
   let errors = [];
-	if (![1, 2, 3, 6].includes(data.data.speed)) {
+	if (data.data.speed && ![1, 2, 3, 6].includes(data.data.speed)) {
 		errors.push(new Error("Speed does not match any mode (1, 2, 3 or 6 are available)"));
 	}
-	if (data.data.angle != data.asteroidData.hardness * 2) {
+	if (data.data.angle && data.data.angle != data.asteroidData.hardness * 2) {
 		errors.push(new Error("Rocket's angle does not fit for this asteroid hardness"));
 	}
-	if (data.data.weight != data.asteroidData.weight * 0.054) {
+	if (data.data.weight && data.data.weight != data.asteroidData.weight * 0.054) {
 		errors.push(new Error("Rocket's weight is not OK according to asteroid's weight"));
 	}
 	if (errors.length > 0) {
--- test-format.js
+++ test-format.js
@@ -7,33 +7,33 @@ try {
   }
 
   let errors = [];
-    if (typeof data.data.speed != "number") {
-      errors.push(new Error("Invalid format for speed property - must be a number"));
+  if (data.data.speed && typeof data.data.speed != "number") {
+    errors.push(new Error("Invalid format for speed property - must be a number"));
+  }
+  if (data.data.temperature && typeof data.data.temperature != "number") {
+      errors.push(new Error("Invalid format for temperature property - must be a number"));
     }
-    if (typeof data.data.temperature != "number") {
-        errors.push(new Error("Invalid format for temperature property - must be a number"));
-      }
-    if (typeof data.data.angle != "number" || data.data.angle > 180 || data.data.angle < 0) {
-        errors.push(new Error("Invalid format for angle property - must be a number between [0, 180]"));
-      }
-	if (data.data.weight > 580 || data.data.weight < 530) {
-		errors.push(new Error("Rocket's weight is out of range"));
+  if (data.data.angle && typeof data.data.angle != "number" || data.data.angle > 180 || data.data.angle < 0) {
+      errors.push(new Error("Invalid format for angle property - must be a number between [0, 180]"));
     }
-    if (typeof data.data.shape != "string") {
-        errors.push(new Error("Invalid format for shape property - must be a string"));
-    }
-    if (typeof data.data.fuel != "string") {
-        errors.push(new Error("Invalid format for fuel property - must be a string"));
-    }
-    if (typeof data.data.launchDate != "string" && checkDateFormat(data.data.launchDate)) {
-        errors.push(new Error("Invalid format for date property - must be DD/MM/YYYY or dd-mm-yyyy or dd.mm.yyyy"));
-    }
-    if (errors.length > 0) {
-      throw errors; 
-    }
+  if (data.data.weight && data.data.weight > 580 || data.data.weight < 530) {
+    errors.push(new Error("Rocket's weight is out of range"));
+  }
+  if (data.data.shape && typeof data.data.shape != "string") {
+      errors.push(new Error("Invalid format for shape property - must be a string"));
+  }
+  if (data.data.fuel && typeof data.data.fuel != "string") {
+      errors.push(new Error("Invalid format for fuel property - must be a string"));
+  }
+  if (data.data.launchDate && typeof data.data.launchDate != "string" && checkDateFormat(data.data.launchDate)) {
+      errors.push(new Error("Invalid format for launchDate property - must be DD/MM/YYYY or dd-mm-yyyy or dd.mm.yyyy"));
+  }
+  if (errors.length > 0) {
+    throw errors; 
+  }
 } catch (errors) {
-    errors.map((e) => {
-      console.log(e.name + ": " + e.message);
-    })
-    process.exit(1)
+  errors.map((e) => {
+    console.log(e.name + ": " + e.message);
+  })
+  process.exit(1)
 }
