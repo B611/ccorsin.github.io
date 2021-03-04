61aba8c648da18ff4e652354e93809f1a5921e15
ccorsin <ccorsin@student.42.fr>
2021-03-04 13:57:00 +0100

    Clear config

--- data.js
+++ data.js
@@ -1,16 +1,10 @@
 const rocketData = {
   "angle": 60,
-  "launchDate": "23/11/1990",
-  "speed": 1,
-  "shape": "sharp",
-  "fuel": "kerosene",
-  "weight": 540,
-  "temperature": 15,
 }
 
 const asteroidData = {
-  "hardness": 4,
-  "weight": 1000,
+  "hardness": 12,
+  "weight": 10,
   "composition": "nickel",
 }
 
