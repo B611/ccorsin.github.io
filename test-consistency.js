1265485922af499c9ce97f78fa91450581ee4283
ccorsin <ccorsin@student.42.fr>
2021-03-04 11:48:04 +0100

    Try new config

--- test-consistency.js
+++ test-consistency.js
@@ -12,7 +12,7 @@ try {
 	if (data.data.weight && data.data.weight != data.asteroidData.weight * 0.54) {
 		errors.push(new Error("Rocket's weight is not OK according to asteroid's weight"));
 	}
-	if (data.data.temperature && data.data.temperature >= 10) {
+	if (data.data.temperature && data.data.temperature <= 10) {
 		errors.push(new Error("Current temperature is to low to launch the rocket !"));
 	}
 	if (errors.length > 0) {
