2f6eea40ec81e78a4ba655d2e296aad7646ebfa5
ccorsin <ccorsin@student.42.fr>
2021-05-19 17:09:06 +0200

    l1

--- data.js
+++ data.js
@@ -1,7 +1,17 @@
 const rocketData = {
+  "angle": 60,
+  "launchDate": "23/11/1990",
+  "speed": 1,
+  "shape": "sharp",
+  "fuel": "kerosene",
+  "weight": 540,
+  "temperature": 15,
 }
 
 const asteroidData = {
+  "hardness": 30,
+  "weight": 1000,
+  "composition": "nickel",
 }
 
 module.exports = {
