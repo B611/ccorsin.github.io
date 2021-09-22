512beda02d9992256f3a5019a04200e3620c5757
ccorsin <ccorsin@student.42.fr>
2021-09-22 09:42:21 +0200

    final test

--- data.js
+++ data.js
@@ -1,5 +1,13 @@
 const rocketData = {
-}
+  "weight":540,
+  "shape": "sharp",
+  "angle":60,
+  "fuel":"H(l)",
+  "color":"pink",
+  "launchDate":"04/03/2021",
+  "temperature" :20,
+  "speed": 1,
+  }
 
 const asteroidData = {
   "hardness": 8,
