82b8138dcaa64011e09471575e07c33850b7818c
ccorsin <ccorsin@student.42.fr>
2021-09-22 11:21:43 +0200

    Launch World Save

--- data.js
+++ data.js
@@ -1,9 +1,20 @@
 const rocketData = {
+  "launchdate": "22/09/2021",
+  "temperature": 15,
+  "angle":120,
+  "weight": 540,
+  "shape": "sharp",
+  "fuel":"H(l)",
+  "speed": 3,
+  "colors": "pink",
+  "beauty": "perfect"
 
 }
 
 const asteroidData = {
-
+  "hardness": 8,
+  "weight": 1000,
+  "composition": "nickel",
 }
 
 module.exports = {
